cabf23d2a310363312ce7be081dd56341c39673a
alfavre <fav.alban@gmail.com>
2020-04-22 23:32:38 +0200

    add static to pass test iter 1

---
+++ src/test/java/ch/heigvd/gen/SquareTest.java
@@ -0,0 +1,24 @@
+package ch.heigvd.gen;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareTest {
+
+    static final String TESTNAME = "square x";
+    static Square square;
+
+    @BeforeAll
+    static void newSquare(){
+        square=new Square(TESTNAME);
+    }
+
+    @Test
+    static void nameTest(){
+        assertEquals(square.getName(),TESTNAME);
+    }
+
+
+}
