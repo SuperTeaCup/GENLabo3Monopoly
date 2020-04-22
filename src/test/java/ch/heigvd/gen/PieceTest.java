65dc6c8cf5df497f2e295eec78f28e5f5a08a019
alfavre <fav.alban@gmail.com>
2020-04-22 23:57:14 +0200

    everithing static in pieceTest

--- src/test/java/ch/heigvd/gen/PieceTest.java
+++ src/test/java/ch/heigvd/gen/PieceTest.java
@@ -7,25 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PieceTest {
 
-    String squareName="squareTest";
-    Piece piece;
-    Square square;
-    Square startSquare;
+    static String squareName="squareTest";
+    static Piece piece;
+    static Square square;
+    static Square startSquare;
 
     @BeforeAll
-    void newPiece(){ //it seems this was not the best name idea for the setting up method
+    static void newPiece(){ //it seems this was not the best name idea for the setting up method
         piece = new Piece();
         square = new Square(squareName);
         startSquare = new Square("GO");
     }
 
     @Test
-    void startPosition(){
+    static void startPosition(){
         assertEquals(piece.getLocation(),startSquare);
     }
 
     @Test
-    void correctDeplacement(){
+    static void correctDeplacement(){
         piece.setLocation(square);
         assertEquals(piece.getLocation(),square);
     }
